ac0834450f3dd1d88c2332a3d9101273468a49e1
Ty Qualters <qualtersty@gmail.com>
2025-06-26 01:42:19 -0500

    add back meme

--- illustrators/src/app/game/page.tsx
+++ illustrators/src/app/game/page.tsx
@@ -4,11 +4,11 @@ import './game.css';
 export default function Game() {
     return (
         <div className="flex items-center flex-col w-screen h-screen">
-            <div className="relative hidden">
+            <div className="relative">
                 <p className="absolute top-0 left-0 meme-text">This game will work, right?!</p>
                 <img id="sweating" src="https://media1.tenor.com/m/Fp0JJdxY6msAAAAC/yes-sweating.gif" alt="gif" />
             </div>
-            <GameCanvas className="flex-grow w-full h-full" />
+            <GameCanvas className="w-full h-full" />
         </div>
     );
 }
