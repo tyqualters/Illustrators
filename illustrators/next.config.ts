662c611604bf2fbe2944d1fd50da17b8d779296b
Ty Qualters <qualtersty@gmail.com>
2025-07-06 21:44:23 -0500

    add these comments for future ref

--- illustrators/next.config.ts
+++ illustrators/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // THIS OPTION WILL DISABLE CODE LINTING ERRORS AND WARNINGS
+  //    (USE FOR DEV PURPOSES ONLY)
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
+  //
+  //
+  // THIS OPTION WILL DISABLE TYPE CHECKING ERRORS AND WARNINGS
+  //    (USE FOR DEV PURPOSES ONLY)
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
 };
 
 export default nextConfig;
