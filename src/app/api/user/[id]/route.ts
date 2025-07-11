ad87bb67b35b548a738ee7af0ee0153a5c387d18
Ty Qualters <qualtersty@gmail.com>
2025-07-10 23:21:33 -0500

    Added REST API for basic profile by ID

---
+++ src/app/api/user/[id]/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from 'next/server'; // imports the Next.js helper for returning responses in API routes
+import connectDB from '@/lib/mongo';
+import User from '@/models/User';
+import type { NextRequest } from 'next/server'; // type import for typed request objects
+import crypto from 'crypto';
+
+async function sha256(input: string): Promise<string> {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(input);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+  return hashHex;
+}
+
+export async function GET(req: NextRequest, context: { params: Promise<{ id: string }> }) {
+  await connectDB();
+  const { params } = await context;  // Await context to get params
+  const { id } = await params;
+
+    // Check for valid Mongo ObjectId format
+  if (!id || id.length !== 24) {
+    return NextResponse.json({ error: 'User ID is malformed.' }, { status: 400 });
+  }
+
+  const user = await User.findById(id);
+  if (!user) {
+    return NextResponse.json({ error: 'User does not exist' }, { status: 404 });
+  }
+ 
+  return NextResponse.json({
+    id: id,
+    name: user?.name,
+    gravatar: `https://gravatar.com/avatar/${ await sha256(user?.email.trim().toLowerCase()) }`
+  });
+}
