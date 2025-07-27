6b85f657711517c5ecff49596858433047d619c4
KFletch117 <kjflet1@ilstu.edu>
2025-07-26 23:04:19 -0500

    Laser Component Added

---
+++ src/app/components/Laser/Laser.jsx
@@ -0,0 +1,50 @@
+'use client'
+import React, { useState, useEffect, useRef } from 'react';
+import './Laser.css';
+
+function Laser() {
+  const [lasers, setLasers] = useState([]);
+  const laserAudio = useRef(null);
+
+  useEffect(() => {
+    laserAudio.current = new Audio('/sounds/laserSound.mp3'); // make sure path is correct
+  }, []);
+
+  const handleClick = (e) => {
+    const newLaser = {
+      id: Date.now(),
+      x: e.clientX,
+      y: e.clientY,
+    };
+
+    setLasers((prev) => [...prev, newLaser]);
+
+    if (laserAudio.current) {
+      laserAudio.current.currentTime = 0;
+      laserAudio.current.play();
+    }
+
+    setTimeout(() => {
+      setLasers((prev) => prev.filter((l) => l.id !== newLaser.id));
+    }, 400); // match your CSS animation duration
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClick);
+    return () => document.removeEventListener('click', handleClick);
+  }, []);
+
+  return (
+    <>
+      {lasers.map((laser) => (
+        <div
+          key={laser.id}
+          className="laser"
+          style={{ left: `${laser.x}px`, top: `${laser.y}px` }}
+        />
+      ))}
+    </>
+  );
+}
+
+export default Laser;
